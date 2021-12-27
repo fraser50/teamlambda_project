a59ae9ca1ae2f65ff1496d788c1db0485ae1044c
Fraser Watson <fjw3@hw.ac.uk>
2021-12-27 14:29:26 +0000

    Fixed links and main page

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const path = require("path");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("<html><body><h1>TODO: add main page</h1></body></html>");
+    res.sendFile(path.join(__dirname, "FrontEndCode/index.html"));
 });
 
 app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "login.html"));
+    res.sendFile(path.join(__dirname, "FrontEndCode/login.html"));
 });
 
 // TODO: Use a static directory for things like stylesheets, images, etc
 app.get("/style.css", function(req, res) {
-    res.sendFile(path.join(__dirname, "style.css"));
+    res.sendFile(path.join(__dirname, "FrontEndCode/style.css"));
 });
 
 app.listen(8080);
